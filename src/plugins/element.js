3a2dd72f7bc3a116c14b89383ded3982464a6066
WowZm <1835780031@qq.com>
2020-08-13 21:22:23 +0800

    完成用户列表功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,51 @@
 import Vue from 'vue'
-import { Form, FormItem, Button, Input,Message } from 'element-ui'
+import { Form, 
+        FormItem, 
+        Button, 
+        Input, 
+        Message, 
+        Container, 
+        Header, 
+        Aside,
+        Main,
+        Menu,
+        Submenu,
+        MenuItem,
+        Breadcrumb,
+        BreadcrumbItem,
+        Card,
+        Row,
+        Col,
+        Table,
+        TableColumn,
+        Switch,
+        Tooltip,
+        Dialog,
+        MessageBox,
+        Pagination
+    } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Dialog)
+Vue.use(Pagination)
 Vue.prototype.$message = Message//挂载到Vue
+Vue.prototype.$confirm = MessageBox.confirm//挂载到Vue
