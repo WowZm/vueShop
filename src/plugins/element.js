930cbc118f8dadc1edc278a61f41777ba4456812
WowZm <1835780031@qq.com>
2020-08-14 17:04:41 +0800

    完成权限功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -22,7 +22,11 @@ import { Form,
         Tooltip,
         Dialog,
         MessageBox,
-        Pagination
+        Pagination,
+        Tag,
+        Tree,
+        Select,
+        Option
     } from 'element-ui'
 
 Vue.use(Button)
@@ -47,5 +51,9 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Dialog)
 Vue.use(Pagination)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
 Vue.prototype.$message = Message//挂载到Vue
 Vue.prototype.$confirm = MessageBox.confirm//挂载到Vue
